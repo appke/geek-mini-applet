a1522240d99eb4001925557d063eb7d1c3b118dc
bn-mul2 <liangmu.gdut@gmail.com>
2020-10-31 19:47:27 +0800

    增加share照片

---
+++ pages/sharephoto/sharephoto.js
@@ -0,0 +1,11 @@
+// pages/sharephoto/sharephoto.js
+Page({
+
+  onShareAppMessage() {
+    return {
+      title: "热门路线",
+      imageUrl: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2334330329,1820500093&fm=26&gp=0.jpg"
+    }
+  }
+})
+// 
