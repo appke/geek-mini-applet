90702e3454d8d99d10a0e2ace1fae55eabca94c2
bn-mul2 <liangmu.gdut@gmail.com>
2020-11-01 12:16:59 +0800

    小程序基础语法完毕

--- pages/sharephoto/sharephoto.js
+++ pages/sharephoto/sharephoto.js
@@ -5,12 +5,12 @@ Page({
     counter: 0
   },
 
-  onShareAppMessage() {
-    return {
-      title: "热门路线",
-      imageUrl: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2334330329,1820500093&fm=26&gp=0.jpg"
-    }
-  },
+onShareAppMessage() {
+  return {
+    title: "热门路线",
+    imageUrl: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2334330329,1820500093&fm=26&gp=0.jpg"
+  }
+},
   handleCrement(event) {
     console.log('------' , this.data, event)
     this.setData({
