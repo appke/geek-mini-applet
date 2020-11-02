bb4c41692262164c93aeac20f614bd538ce18ae4
bn-mul2 <liangmu.gdut@gmail.com>
2020-11-02 15:04:39 +0800

    封装网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,11 @@
 // pages/home/home.js
-Page({
 
+// import getMutiData from '../../service/home.js'
+import {
+  getMultiData
+} from '../../service/home.js'
+
+Page({
   /**
    * 页面的初始数据
    */
@@ -8,59 +13,11 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    // 请求轮播图、以及推荐数据
+    getMultiData().then(res => {
+      console.log('res----', res)
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
